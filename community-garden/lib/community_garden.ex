fe592354e8fd4a5e2db666bcd78ca1b4931feedd
OOI JIA YI <ooijiayi93@gmail.com>
2024-07-07 15:58:46 +0800

    community plot solution

--- community-garden/lib/community_garden.ex
+++ community-garden/lib/community_garden.ex
@@ -1,27 +1,37 @@
-# Use the Plot struct as it is provided
 defmodule Plot do
   @enforce_keys [:plot_id, :registered_to]
   defstruct [:plot_id, :registered_to]
 end
 
 defmodule CommunityGarden do
-  def start(opts) do
-    # Please implement the start/1 function
+  def start(opts \\ []) do
+    Agent.start(fn -> %{plots: [], next_plot_id: 1} end, opts)
   end
 
   def list_registrations(pid) do
-    # Please implement the list_registrations/1 function
+    Agent.get(pid, fn state -> state.plots end)
   end
 
   def register(pid, register_to) do
-    # Please implement the register/2 function
+    Agent.get_and_update(pid, fn state ->
+      new_plot = %Plot{plot_id: state.next_plot_id, registered_to: register_to}
+      {new_plot, %{plots: [new_plot | state.plots], next_plot_id: state.next_plot_id + 1}}
+    end)
   end
 
   def release(pid, plot_id) do
-    # Please implement the release/2 function
+    Agent.get_and_update(pid, fn state ->
+      {:ok, %{state | plots: Enum.filter(state.plots, fn plot -> plot.plot_id != plot_id end)}}
+    end)
   end
 
   def get_registration(pid, plot_id) do
-    # Please implement the get_registration/2 function
+    Agent.get(pid, fn state ->
+      Enum.find(
+        state.plots,
+        {:not_found, "plot is unregistered"},
+        fn plot -> plot.plot_id == plot_id end
+      )
+    end)
   end
 end
--- top-secret/lib/top_secret.ex
+++ top-secret/lib/top_secret.ex
@@ -1,11 +1,15 @@
 defmodule TopSecret do
   def to_ast(string) do
-    # Please implement the to_ast/1 function
+    Code.string_to_quoted!(string)
   end
 
-  def decode_secret_message_part(ast, acc) do
-    # Please implement the decode_secret_message_part/2 function
+  def decode_secret_message_part({operation, _metadata, arguments} = node, acc)
+      when operation in [:def, :defp] do
+    {functiona_name, _, _} = List.first(arguments)
+    {node, [Atom.to_string(functiona_name) | acc]}
   end
+
+  def decode_secret_message_part(node, acc), do: {node, acc}
 
   def decode_secret_message(string) do
     # Please implement the decode_secret_message/1 function
